510a2c47a9661482e5074423da88c6fca662a3f5
szscnc <88357194+szscnc@users.noreply.github.com>
2025-02-08 10:40:11 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,8 @@
-export interface Proxy {
-       domain: string;
-       url: string;
-}
+import type {Proxy} from './util/model';
 
-export let proxys:Proxy[]= [
-        {
+export let PORT = 377; // 端口
+
+export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
             domain:"easy-reverse-proxy-topaz.vercel.app"
-            url:"https://www.google.com/"
-}
-};
+            url:"https://www.google.com/"// 代理
+export let PROXIES: Proxy[] = [];
