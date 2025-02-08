10581be91aa39c7052d6f96a559862063f780ef6
szscnc <88357194+szscnc@users.noreply.github.com>
2025-02-08 10:32:02 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,7 @@
-export interface Proxy {
-       domain: string;
-       url: string;
-}
+import type {Proxy} from './util/model';
 
-export let proxys:Proxy[]= [
-    {
-        domain:"easy-reverse-proxy-bybh.vercel.app"
-        url:"https://www.google.com/"
-    }
-};
+export let PORT = 377; // 端口
+
+export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
+// 代理
+export let PROXIES: Proxy[] = [];
