c7e3ab1b7afbf0e63260db1aa473e0c387918714
szscnc <88357194+szscnc@users.noreply.github.com>
2025-02-08 10:36:18 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,3 @@
-import type {Proxy} from './util/model';
-
-export let PORT = 377; // 端口
-
-export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
-// 代理
-export let PROXIES: Proxy[] = [];
 export interface Proxy {
        domain: string;
        url: string;
